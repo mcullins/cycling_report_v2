2f608cc13dc478b736f6cba9ea410bc1e8c2f5eb
Meg <40369841+mcullins@users.noreply.github.com>
2020-02-23 11:00:20 -0600

    made data appear on correct date only

--- src/main/java/com/cyclingrecord/controllers/MonthlyTableController.java
+++ src/main/java/com/cyclingrecord/controllers/MonthlyTableController.java
@@ -36,11 +36,11 @@ public class MonthlyTableController {
     @RequestMapping("monthly")
     public String showMonthlyTable(Model model, Entry newEntry, String date){
 
-        for (int i=0; i <= getCurrentMonth().size()-1; i++){
-            if (getCurrentMonth().get(i).equals(date)){
-   //             EntryData.add(newEntry);
-            }
-        }
+//        for (int i=0; i <= getCurrentMonth().size()-1; i++){
+//            if (getCurrentMonth().get(i).equals(date)){
+//                EntryData.add(newEntry);
+//            }
+//        }
 
         model.addAttribute("month", getCurrentMonth());
         return "monthly";
